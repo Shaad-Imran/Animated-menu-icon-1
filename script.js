43cfef32592148d8a43a3573e63f00ed807c14f6
Imran <imranmansuri038@gmail.com>
2022-08-25 19:27:39 +0530

    commit initiate

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu1 = document.querySelector(".menu1");
+menu1.addEventListener("click", () => {
+  menu1.classList.toggle("openmenu1");
+});
+
+const menu2 = document.querySelector(".menu2");
+menu2.addEventListener("click", () => {
+  menu2.classList.toggle("openmenu2");
+});
